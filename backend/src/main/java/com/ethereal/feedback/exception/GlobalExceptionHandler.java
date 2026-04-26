8fe7f1f20cbf4f72e36d474dad87a8573a8ab262
geezdock <dhruvav0308@gmail.com>
2026-04-26 13:39:17 +0530

    Add debug output to exception handler

--- backend/src/main/java/com/ethereal/feedback/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ethereal/feedback/exception/GlobalExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.ethereal.feedback.exception;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,6 +16,9 @@ public class GlobalExceptionHandler {
         Map<String, String> response = new HashMap<>();
         response.put("error", "The void encountered a disturbance.");
         response.put("message", "We are recalibrating the sanctuary. Please try again in a moment.");
+        response.put("debug", ex.getMessage());
+        
+        ex.printStackTrace();
         
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
